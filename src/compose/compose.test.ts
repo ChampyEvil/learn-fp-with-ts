66a43cf08d3317d69d40a57ae4b3bb5d0ccdf97d
Champ <champ@odds.team>
2025-07-05 10:59:13 +0700

    lab 1: Implement to solve test 1 and 2

--- src/compose/compose.test.ts
+++ src/compose/compose.test.ts
@@ -1,12 +1,13 @@
 // import { compose, increment, incrementThenToString, toString } from "./compose"
+import { increment, toString } from "./compose"
 
 describe('Compose in FP', () => {
   it('should increment number', () => {
-    // expect(increment(1)).toBe(2)
+    expect(increment(1)).toBe(2)
   })
 
   it('should convert number to string', () => {
-    // expect(toString(1)).toBe('1')
+    expect(toString(1)).toBe('1')
   })
 
   it('should increment then convert number to string', () => {
--- src/compose/compose.ts
+++ src/compose/compose.ts
@@ -1,1 +1,7 @@
-// Start here...
+type Increment = (x: number) => number
+const increment: Increment = x => x + 1
+
+type ToString = (x: number) => string
+const toString: ToString = x => `${x}`
+
+export { increment, toString }

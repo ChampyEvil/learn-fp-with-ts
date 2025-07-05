c7b3961063cc5c1b8ac1f2d420955a7870da4b7f
Champ <champ@odds.team>
2025-07-05 12:13:20 +0700

    lab:3 option: implement some and none for solve all test

--- src/option/options.test.ts
+++ src/option/options.test.ts
@@ -1,7 +1,7 @@
 // import {
 //   composed, composed2, divideTwo, divideTwo2, isNone, none, some
 // } from "./options"
-import { composed, divideTwo } from "./options"
+import { composed, divideTwo, some, none, isNone, divideTwo2, composed2 } from "./options"
 
 describe('Options', () => {
   it('should divide 2 with number that not zero', () => {
@@ -18,25 +18,25 @@ describe('Options', () => {
   })
 
   it('should convert any type of A to Some of type A', () => {
-    // expect(some(1)).toEqual({_tag: "some", value: 1})
+    expect(some(1)).toEqual({_tag: "some", value: 1})
   })
 
   it('should get Option of never from none function', () => {
-    // expect(none).toEqual({ _tag: 'None' })
+    expect(none).toEqual({ _tag: 'none' })
   })
 
   it('should tell that option is some or none', () => {
-    // expect(isNone(some(1))).toBe(false)
-    // expect(isNone(none)).toBe(true)
+    expect(isNone(some(1))).toBe(false)
+    expect(isNone(none)).toBe(true)
   })
 
   it('DivideTwo should return Some or None', () => {
-    // expect(divideTwo2(2)).toEqual(some(1))
-    // expect(divideTwo2(0)).toEqual(none)
+    expect(divideTwo2(2)).toEqual(some(1))
+    expect(divideTwo2(0)).toEqual(none)
   })
 
   it('should be able to compose with Option type', () => {
-    // expect(composed2(2)).toEqual(some(2))
-    // expect(composed2(0)).toEqual(none)
+    expect(composed2(2)).toEqual(some(2))
+    expect(composed2(0)).toEqual(none)
   })
 })
--- src/option/options.ts
+++ src/option/options.ts
@@ -10,7 +10,28 @@ const increment = (x: number) => x + 1
 
 const composed = compose(increment, divideTwo)
 
+type Option<A> = Some<A> | None
+interface Some<A> {
+  _tag: 'some',
+  value: A
+}
+interface None {
+  _tag: 'none'
+}
+
+const some = <A>(a: A): Option<A> => ({ _tag: 'some', value: a })
+const none: Option<never> = { _tag: 'none' }
+
+const isNone = <A>(x: Option<A>): x is None => x._tag === 'none'
+
+type DivideTwo2 = (x: number) => Option<number>
+const divideTwo2: DivideTwo2 = (x) => x === 0 ? none : some(2 / x)
+
+const composed2 = compose(
+  (x: Option<number>) => isNone(x) ? none : some(increment(x.value)),
+  divideTwo2
+)
+
 export {
-  divideTwo, composed, 
-  // some, none, isNone, divideTwo2, composed2
+  divideTwo, composed, some, none, isNone, divideTwo2,composed2
 } 
